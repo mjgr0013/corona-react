7a419217677d8f11681e0da779c402d078433cb7
manuel.garcia <mjgr0013@gmail.com>
2020-04-12 12:03:35 +0200

    Updates

---
+++ precache-manifest.1dfa280d3caa39b54fcfb1add09736a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "395fb6c574e00a19bb81836735b7721a",
+    "url": "/film-app/index.html"
+  },
+  {
+    "revision": "6176237efd5f2093099f",
+    "url": "/film-app/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0622bbfe5f7b5b85554d",
+    "url": "/film-app/static/js/2.73640374.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/film-app/static/js/2.73640374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6176237efd5f2093099f",
+    "url": "/film-app/static/js/main.54ed0458.chunk.js"
+  },
+  {
+    "revision": "f9cf6f1bee5f7e5c0e88",
+    "url": "/film-app/static/js/runtime-main.a9ba0d80.js"
+  }
+]);
